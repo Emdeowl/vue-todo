f79641a155026124bd742ff4ae80049062ad8ba0
JaeHyuk <kwon850550@naver.com>
2022-08-20 21:18:55 +0900

    : function 축약

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,22 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  data:function(){
+  data(){
     return{
       todoitems:[]  // 할 일 데이터
     }
   },
   methods:{
-    addOneItem:function(todoItem){
+    addOneItem(todoItem){
          const obj={completed:false, item:todoItem};  //completed를 통하여 상태 부여
         localStorage.setItem(todoItem,JSON.stringify(obj));  // JSON.stringify는 obj라는 객체값을 문자열로 바꿔준다.
         this.todoitems.push(obj); 
     },
-    removeOneItem:function(todoitem,index){
+    removeOneItem(todoitem,index){
        this.todoitems.splice(index,1); // 선택한 index의 배열의 index에서 splice를 통하여 삭제한다. 
       localStorage.removeItem(todoitem.item); // 로직에 있는 obj안 item을 제거
     },
-    toggleOneItem:function(todoItem,index){
+    toggleOneItem(todoItem,index){
       //  todoItem.completed= !todoItem.completed;
         this.todoitems[index].completed = !this.todoitems[index].completed
         
@@ -38,12 +38,12 @@ export default {
       localStorage.setItem(todoItem.item,JSON.stringify(todoItem));
       // 로직에서 변경을 하기 위해서  삭제->생성
     },
-    clearAllItem:function(){
+    clearAllItem(){
        localStorage.clear();
        this.todoitems=[];
     }
   },
-  created:function(){
+  created(){
     if(localStorage.length>0){
       for(let i=0;i<localStorage.length ;i++) //var -> let으로 변환, 반복문은 const로 하면 돌아가지 않음 재 할당이 안되기 떄문이다.
       {
@@ -75,6 +75,7 @@ input{
   border-style : groove;
   width: 200px;
 }
+
 button{
   border-style: groove;
 }
